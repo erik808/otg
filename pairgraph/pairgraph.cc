592323e7cba5bb4a11a6ea776d09123603aa070f
Joren Heit <jorenheit@gmail.com>
2015-12-05 23:01:40 +0100

    added mapping output to pairgraph

--- pairgraph/pairgraph.cc
+++ pairgraph/pairgraph.cc
@@ -165,24 +165,37 @@ AdjacencyMatrix makeAdjacencyMatrix(CipherPairMatrix const &matrix, CipherPairMa
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc < 5)
     {
-        std::cerr << "Syntax: " << argv[0] << " [input file] [repeat length]\n";
+        std::cerr << "Syntax: " << argv[0] << " [input file] [repeat length] [adjacency out] [mapping out]\n";
         return 1;
     }
 	
     std::ifstream infile(argv[1]);
-    int repeatLength = std::stoi(argv[2]);
+    if (!infile)
+    {
+        std::cerr << "Could not open input file " << argv[1] << '\n';
+        return 1;
+    }
 
+
+    int repeatLength = std::stoi(argv[2]);
     CipherPairMatrix pairMatrix = makeCipherPairMatrix(infile, repeatLength);
     CipherPairMap pairMap = makeCipherPairMap(pairMatrix);
     AdjacencyMatrix adj = makeAdjacencyMatrix(pairMatrix, pairMap);
 
+    std::ofstream adjOut(argv[3]);
     for (auto const &row: adj)
     {
         for (auto const &element: row)
-            std::cout << element << ' ';
-        std::cout << '\n';
+            adjOut << element << ' ';
+        adjOut << '\n';
+    }
+
+    std::ofstream mapOut(argv[4]);
+    for (auto const &pr: pairMap)
+    {
+        mapOut << pr.first << ' ' << pr.second << '\n';
     }
 
 }
